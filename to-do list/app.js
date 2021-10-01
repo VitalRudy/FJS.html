4d3e28bd463e5012ba59d027b1d4309d9408bf6f
Vitalii Shutov <66096506+VitalRudy@users.noreply.github.com>
2021-10-01 23:44:30 +0800

    Update app.js

--- to-do list/app.js
+++ to-do list/app.js
@@ -1,1 +1,33 @@
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
 
+//Event Listeners
+todoButton.addEventListener("click", addTodo);
+
+//Functions
+function addTodo(event) {
+  //Prevent form from submitting
+  event.preventDefault();
+  //Todo DIV
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //Create LI
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "Hey";
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  //check mark button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class ="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  //check trash button
+  const trashdButton = document.createElement("button");
+  trashdButton.innerHTML = '<i class ="fas fa-trash"></i>';
+  trashdButton.classList.add("complete-btn");
+  todoDiv.appendChild(trashdButton);
+  //Append to list
+  todoList.appendChild(todoDiv);
+}
